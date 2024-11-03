3903c6d79cb7a6997f10f60f244c2bfd9cb2e062
cdrmack <git.pynvt@passmail.net>
2024-11-03 16:21:53 +0100

    replace text with rectangle in c_raylib

--- c_raylib/main.c
+++ c_raylib/main.c
@@ -1,14 +1,20 @@
 #include <raylib.h>
 
+const int kWindowWidth = 800;
+const int kWindowHeight = 600;
+
 int main(int argc, char **argv)
 {
-    InitWindow(800, 600, "TITLE");
+    InitWindow(kWindowWidth, kWindowHeight, argv[0]);
+
+    const Rectangle rect = {
+        .x = 20.0, .y = 20.0, .width = 100.0, .height = 100.0};
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawText(argv[0], 40, 40, 20, WHITE);
+        DrawRectangleRec(rect, WHITE);
         EndDrawing();
     }
 
